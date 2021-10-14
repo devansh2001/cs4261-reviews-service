8d662bfd5ff1d75ba668616b4fa7183c70050f2c
tejas-pradeep <rptejas@gmail.com>
2021-10-14 02:16:27 -0400

    Adding in logic to reviews api

--- app.py
+++ app.py
@@ -37,47 +37,53 @@ def health_check():
 @app.route('/create-reveiw', methods=['POST'])
 def create_review():
     pass
-    # # https://stackoverflow.com/a/67461897
-    # data = request.get_json()
-    # service_id = str(uuid.uuid4())
-    # service_name = data['service_name']
-    # service_description = data['service_description']
-    # service_category = data['service_category']
-    # query = '''
-    #     INSERT INTO services (service_id, service_name, service_description, service_category)
-    #     VALUES (%s, %s, %s, %s)
-    # '''
-    # cursor.execute(query, [service_id, service_name, service_description, service_category])
-    # conn.commit()
-    # return {'status': 201, 'service_id': service_id}
+    # https://stackoverflow.com/a/67461897
+    data = request.get_json()
+    review_id = str(uuid.uuid4())
+    consumer_id = data['consumer_id']
+    provider_id = data['provider_id']
+    review_text = data['review_text']
+    review_rating = data['review_rating']
+    query = '''
+        INSERT INTO reviews (review_id, review_text, review_rating, provider_id, consumer_id)
+        VALUES (%s, %s, %s, %s, %s)
+    '''
+    cursor.execute(query, [review_id, review_text, review_rating, provider_id, consumer_id])
+    conn.commit()
+    return {'status': 201, 'service_id': review_id}
 
 @app.route('/delete-review/<review-id>', methods=['DELETE'])
 def delete_review(review_id):
-    pass
-    # query = '''
-    #     SELECT service_id, service_name, service_description, service_category FROM services WHERE services.service_id=%s
-    # '''
-    # cursor.execute(query, [str(service_id)])
-    # res = cursor.fetchall()
-    # if (len(res) == 0):
-    #     return {'status': 200, 'service': None}
-    # service = {
-    #     'service_id': res[0][0],
-    #     'service_name': res[0][1],
-    #     'service_description': res[0][2],
-    #     'service_category': res[0][3]
-    # }
-    # return {'status': 200, 'service': service}
+    query = '''
+        DELETE FROM reviews WHERE review_id=%s
+    '''
+    cursor.execute(query, [str(review_id)])
+    conn.commit()
+    return {'status': 200}
 
 @app.route('/get-all-reveiws/<user_id>')
-def delete_service(service_id):
-    pass
-    # query = '''
-    #     DELETE FROM services WHERE services.service_id=%s
-    # '''
-    # cursor.execute(query, [str(service_id)])
-    # conn.commit()
-    # return {'status': 200}
+def delete_service(user_id):
+    query = '''
+            SELECT * FROM reviews WHERE provider_id=%s
+        '''
+    cursor.execute(query, [str(user_id)])
+    res = cursor.fetchAll()
+    return {'status': 200, 'reviews': publish_reviews(res)}
+
+# Helper functions
+def publish_reviews(reviews):
+    if len(reviews) == 0:
+        return None
+    res = []
+    for r in reviews:
+        res.append({
+            'review_id': r[0],
+            'review_text': r[1],
+            'review_rating': r[2],
+            'provider_id': r[3],
+            'consumer_id': r[4]
+        })
+    return res
 
 # https://www.youtube.com/watch?v=4eQqcfQIWXw
 if __name__ == '__main__':
